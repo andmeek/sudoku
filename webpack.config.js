6ab3d3c08bcb4abd141256c65edf6ab17179d9fc
Steven Meek <andmeek@gmail.com>
2019-06-18 18:55:17 -0700

    Add basic Vue shell, run on webpacker server

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 8080
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+};
