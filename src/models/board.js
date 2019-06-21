b1f0a3771e67a0def2051b47249c7bf00c212adb
Steven Meek <andmeek@gmail.com>
2019-06-21 09:00:30 -0700

    Add Board object to represent the state of a given board and query its tiles

---
+++ src/models/board.js
@@ -0,0 +1,50 @@
+import Tile from './tile.js'
+
+export default class Board {
+  constructor(values, state) {
+    this.tiles = []
+    for(var y = 0; y < values.length; y++) {
+      for(var x = 0; x < values[y].length; x++) {
+        var tile = new Tile(x, y, values[y][x], state[y][x] == 1)
+          this.tiles.push(tile)
+      }
+    }
+  }
+
+  get completed() {
+    return this.tiles.reduce((tot, tile) => {
+        return tot && tile.completed
+    })
+  }
+
+  numberCompleted(number) {
+    return this.tiles
+      .filter((tile) => {
+        return tile.actualValue == number
+      }).reduce((tot, tile) => {
+        return tot && tile.completed
+      })
+  }
+
+  tileAt(x, y) {
+    for(var i in this.tiles) {
+      if(this.tiles[i].x == x && this.tiles[i].y == y) {
+        return this.tiles[i]
+      }
+    }
+
+    return null
+  }
+
+  tilesAtSection(x, y) {
+    let baseX = x * 3, baseY = y * 3, maxX = baseX + 2, maxY = baseY + 2
+    var ret = []
+    for(var i in this.tiles) {
+      var tile = this.tiles[i]
+      if(tile.x >= baseX && tile.x <= maxX && tile.y >= baseY && tile.y <= maxY) {
+        ret.push(tile)
+      }
+    }
+    return ret
+  }
+}
---
+++ test/models/board.test.js
@@ -0,0 +1,89 @@
+import Board from '../../src/models/board.js'
+import Tile from '../../src/models/tile.js'
+import {TEST_BOARD, TEST_BOARD_STATE} from '../variables.js'
+
+describe('Board', () => {
+  var board;
+
+  beforeEach(() => {
+    board = new Board(TEST_BOARD, TEST_BOARD_STATE)
+  })
+
+  test('initialization', () => {
+    expect(board.tiles.length).toEqual(81)
+    expect(board.tiles[0]).toBeInstanceOf(Tile)
+    expect(board.tiles[0].actualValue).toEqual(4)
+    expect(board.tiles[0].userEditable).toEqual(false)
+
+    var lastTile = board.tiles[80]
+
+    expect(lastTile.actualValue).toEqual(9)
+    expect(lastTile.x).toEqual(8)
+    expect(lastTile.y).toEqual(8)
+    expect(lastTile.userEditable).toEqual(false)
+  })
+
+  describe('.completed', () => {
+    test('is false', () => {
+      expect(board.completed).toBeFalsy()
+    })
+
+    test('is true when all the tiles are completed', () => {
+      board.tiles.forEach((tile) => {
+        tile.userValue = tile.actualValue
+      })
+
+      expect(board.completed).toBeTruthy()
+    })
+  })
+
+  describe('.numberCompleted', () => {
+    test('is false', () => {
+      expect(board.numberCompleted(1)).toBeFalsy()
+    })
+
+    test('is true when tiles for that number are completed', () => {
+      board.tiles.forEach((tile) => {
+        if(tile.actualValue == 1) {
+          tile.userValue = tile.actualValue
+        }
+      })
+
+      expect(board.numberCompleted(1)).toBeTruthy()
+      expect(board.numberCompleted(2)).toBeFalsy()
+    })
+  })
+
+  describe('.tileAt', () => {
+    test('returns the expected tile', () => {
+      var tile = board.tileAt(2, 2)
+
+      expect(tile.x).toEqual(2)
+      expect(tile.y).toEqual(2)
+      expect(tile.actualValue).toEqual(7)
+    })
+
+    test('returns null if the x,y doesnt exist', () => {
+      expect(board.tileAt(20, 20)).toBeNull()
+    })
+  })
+
+  describe('.tilesAtSection', () => {
+    test('returns the expected tiles in order of left to right and top to bottom as a 1 dimension array', () => {
+      var tiles = board.tilesAtSection(0, 0)
+      var vals = tiles.map((tile) => { return tile.actualValue })
+
+      expect(vals).toEqual([4, 3, 5, 6, 8, 2, 1, 9, 7])
+
+      tiles = board.tilesAtSection(1, 1)
+      vals = tiles.map((tile) => { return tile.actualValue })
+
+      expect(vals).toEqual([1, 9, 5, 6, 8, 2, 7, 4, 3])
+
+      tiles = board.tilesAtSection(2, 2)
+      vals = tiles.map((tile) => { return tile.actualValue })
+
+      expect(vals).toEqual([8, 7, 4, 1, 3, 6, 2, 5, 9])
+    })
+  })
+})
