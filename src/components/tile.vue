a502430e6c8f4df2f0f4309cec938f99225a1366
Steven Meek <andmeek@gmail.com>
2019-06-21 19:45:59 -0700

    Add more decorations to the tile view

--- src/components/tile.vue
+++ src/components/tile.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="tile">
+  <div class="tile"
+       v-bind:class="{
+       selected: selected,
+       'selected-sibling': selectedSibling,
+       'current-input': currentInput,
+       error: tile.error,
+       }"
+       v-on:mouseover="$emit('tilefocus', tile)"
+       v-on:mouseout="$emit('tilefocus', null)"
+       v-on:click="$emit('tileclick', tile)">
     <span v-if="tile.userEditable && tile.userValue != null" class="user-value">{{ tile.userValue }}</span>
+    <span v-else-if="tile.userDrafts.length > 0" class="draft-values">
+      <span v-for="v in sortedDrafts">{{ v }}</span>
+    </span>
     <span v-else-if="tile.userEditable" class="user-value">&nbsp;</span>
     <span v-else>{{ tile.actualValue }}</span>
   </div>
@@ -8,6 +20,40 @@
 
 <script>
 export default {
-  props: ['board', 'tile'],
+  props: ['board', 'tile', 'currentInput', 'selected', 'selectedSibling'],
+  computed: {
+    sortedDrafts() {
+      return this.tile.userDrafts.sort()
+    }
+  }
 }
 </script>
+
+<style>
+.tile:hover {
+  background-color: #aaa;
+}
+.tile.selected {
+  background-color: #ccc;
+}
+.tile.error, .tile.error .user-val {
+  color: red;
+}
+.tile.selected-sibling {
+  background-color: #eee;
+}
+.tile.current-input {
+  font-weight: bold;
+}
+.tile .user-val {
+  color: #888;
+}
+.tile .draft-values {
+  font-size: 0.3em;
+  position: fixed;
+  color: #555;
+}
+.tile .draft-values span {
+  padding: 0px 2px;
+}
+</style>
--- test/components/tile.test.js
+++ test/components/tile.test.js
@@ -41,4 +41,48 @@ describe('Tile.vue', () => {
 
     expect(wrapper.find('.user-value').text()).toEqual('')
   })
+
+  test('renders the draft values if set and no user value', () => {
+    tile.userValue = null
+    tile.userDrafts = [3, 2, 1]
+    wrapper.vm.$forceUpdate()
+    const spans = wrapper.find('.draft-values').findAll('span')
+
+    expect(spans.at(1).text()).toEqual("1")
+    expect(spans.at(2).text()).toEqual("2")
+    expect(spans.at(3).text()).toEqual("3")
+  })
+
+  test('emits a "tileclick" event when the tile is clicked', () => {
+    wrapper.find('div').trigger('click')
+
+    expect(wrapper.emitted().tileclick).toBeTruthy()
+    expect(wrapper.emitted().tileclick[0]).toEqual([tile])
+  })
+
+  test('emits a "tilefocus" event when the tile is hovered', () => {
+    wrapper.find('div').trigger('mouseover')
+
+    expect(wrapper.emitted().tilefocus).toBeTruthy()
+    expect(wrapper.emitted().tilefocus[0]).toEqual([tile])
+  })
+
+  test('emits a "tilefocus" event with a null tile when the mouse leaves it', () => {
+    wrapper.find('div').trigger('mouseout')
+
+    expect(wrapper.emitted().tilefocus).toBeTruthy()
+    expect(wrapper.emitted().tilefocus[0]).toEqual([null])
+  })
+
+  test('adds css classes based upon the data state', () => {
+    tile.userValue = tile.actualValue + 1
+
+    wrapper.setProps({selected: true, selectedSibling: true, currentInput: true})
+    let div = wrapper.find('div')
+
+    expect(div.classes()).toContain('selected')
+    expect(div.classes()).toContain('selected-sibling')
+    expect(div.classes()).toContain('current-input')
+    expect(div.classes()).toContain('error')
+  })
 })
